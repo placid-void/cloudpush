fa65f0061fabc56af41f2905106bb938165335c9
John Tracy <placid@popos.trusted.home.placid.thevo.id>
2020-06-24 16:51:48 -0600

    Added useful scripts from ajkis/scripts

---
+++ roles/host-setup/files/plex-analyze-cli.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# https://github.com/ajkis/scripts
+# Plex analayze all files that are missing analyzation info
+# OS: Ubunti 16.04 ( in case of other OS's make sure to change paths )
+
+from subprocess import call
+import os
+import requests
+import sqlite3
+import sys
+
+conn = sqlite3.connect('/opt/appdata/plex/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db')
+
+c = conn.cursor()
+c.execute('Select media_items.metadata_item_id As id, metadata_items.title As title From media_items Inner Join metadata_items On media_items.metadata_item_id = metadata_items.id Where media_items.bitrate Is Null And Not metadata_items.metadata_type = "12"')
+items = c.fetchall()
+conn.close()
+
+print("To analyze: " + str( len(items) ))
+
+for row in items:
+	os.system('LD_LIBRARY_PATH=/usr/lib/plexmediaserver PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR=/var/lib/plexmediaserver/Library/Application\ Support /usr/lib/plexmediaserver/Plex\ Media\ Scanner -a -o ' + str(row[0]))
+	os.system('sleep 1')
+	print(str(row[0]))
---
+++ roles/host-setup/files/plex-analyzedeeply-cli.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# https://github.com/ajkis/scripts
+# analyzedeeply.cli will preform deep analyzation only on files that were not processed yet
+from subprocess import call
+import os
+import requests
+import sqlite3
+import sys
+import time
+
+conn = sqlite3.connect('/opt/appdata/plex/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db')
+
+c = conn.cursor()
+c.execute('select meta.id from metadata_items meta \
+join media_items media on media.metadata_item_id = meta.id \
+join media_parts part on part.media_item_id = media.id \
+where part.extra_data not like "%deepAnalysisVersion=2%" \
+and meta.metadata_type in (1, 4, 12) and part.file != "";')
+
+items = c.fetchall()
+conn.close()
+
+print("ANALYZE DEEPLY START")
+print("Number of video files to process: " + str( len(items) ))
+start_time = time.time()
+
+for row in items:
+    print ("Processing video id: " + str(row[0]))
+    start_row = time.time()
+    os.system('LD_LIBRARY_PATH=/usr/lib/plexmediaserver \
+               PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR=/opt/appdata/plex/Library/Application\ Support \
+               /usr/lib/plexmediaserver/Plex\ Media\ Scanner --analyze-deeply -o ' + str(row[0]))
+    end_row = time.time()
+    print ("Video id:" + str(row[0]) + " took {:.2f} seconds to process".format(end_row - start_row))
+end_time = time.time()
+print ("Files proccessed:" + str(row[0]) + " in {:.2f} seconds".format(end_row - start_row))
